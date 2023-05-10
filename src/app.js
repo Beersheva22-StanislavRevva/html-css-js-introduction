a9f2792a4b6a63a68e116ff5df46eeffe2a73bfd
stanislav-revva <nivadrive1@gmail.com>
2023-05-10 11:52:29 +0300

    coomit 10-05-2023

--- src/app.js
+++ src/app.js
@@ -62,36 +62,94 @@
 //console.log((255).toString(16));
 //console.log(("A" + (+"A") + "A" + "S").toLowerCase());
 
-console.log(a = (-123456789).toString(16));
-function myToStringInteger(number, radix) {
-    //TODO: number - any number
-    //radix - computation base, if radix < 2 || radix > 36 then radix = 10
-    //removes fraction part 5/4, ex - 34.25 = 34; 34.75 = 35;
-    //toString method is diallowed
-    //returns string as a view presentation of the integer part of a given number in accordance
-    finalString = "";
-    let flag = 0;
-    if (number < 0) {
-        number = Math.abs(number);
-        flag = 1;
+// function myToStringInteger(number, radix) {
+//     //TODO: number - any number
+//     //radix - computation base, if radix < 2 || radix > 36 then radix = 10
+//     //removes fraction part 5/4, ex - 34.25 = 34; 34.75 = 35;
+//     //toString method is diallowed
+//     //returns string as a view presentation of the integer part of a given number in accordance
+//     const sign = number < 0 ? '-' : '';
+//     number = Math.abs(number);
+//     number = Math.round(number);
+//     if (radix < 2 || radix > 36 ) {
+//         radix = 10;
+//     }
+//     let res = '';
+//     do {
+//         res = getSymbol(number, radix) + res;
+//         number = Math.trunc(number / radix);
+//     } while (number != 0);
+//     return sign + res;
+// }
+// function getSymbol(number, radix) {
+//     const aCode = 'a'.charCodeAt(0);
+//     const delta = aCode - 10;
+//     const remainder = number % radix;
+//     return remainder < 10 ? remainder + '' : String.fromCharCode(remainder + delta);
+// }
+// console.log((123456789).toString(36));
+// console.log(myToStringInteger(123456789, 36));
+// console.log(myToStringInteger(-123456789.1234, 36));
+// console.log(myToStringInteger(0, 36));
+// "string" or 'string' with no string interpolation 'a' - string
+// `...${<expression>}...`
+const strNum = '-0xff';
+let radix;
+//console.log(`string with number ${strNum} with radix ${radix} is ${parseInt(strNum, radix)}`);
+function myParseInt(strNum, radix) {
+    // the same behavior as standard parseInt;
+    strNum = strNum.trim();
+    let index = strNum.charAt(0) == '-' || strNum.charAt(0) == '+' ? 1 : 0;
+    if ((!radix || radix == 16) & getHexdecimalIndex(strNum.substring(index)) > 0) {
+        index += 2;
+        radix = 16;
     }
-
-    if (radix < 2 || radix > 36) {
+    if (!radix) {
         radix = 10;
     }
-
-    do {
-        remainder = Math.floor(Math.floor(number) % radix);
-        if (remainder > 9) {
-            remainder = String.fromCharCode(remainder + 87);
+    // if (typeof(radix) === 'undefined') {
+    //     radix = 10;
+    //     if (strNum.charAt(index) == '0' && strNum.charAt(index + 1) == 'x') {
+    //         index += 2;
+    //         radix = 16;
+    //     }
+    // }
+    radix = typeof(radix) === 'undefined'? 10 : radix;
+    let res = radix > 1 && radix < 37 ? getDigitCode(strNum, index, radix) : NaN;
+    if(!isNaN(res)) {
+        let digit;
+        index++;
+        while (index < strNum.length && !isNaN(digit = getDigitCode(strNum, index, radix))){
+            res = res * radix + digit;
+            index++;
         }
-        finalString = remainder + finalString;
-        number = Math.floor(number / radix);
-    } while (number > 0);
-    if (flag == 0) {
-        return (finalString);
-    } else {
-        return ("-" + finalString);
+        if(strNum[0] == '-') {
+            res = -res;
+        }
     }
+    return res;
 }
-console.log(myToStringInteger(-123456789, 16));
+function getHexdecimalIndex(str) {    
+    return str.toLowerCase().startsWith('0x') ? 2 : 0;
+}
+function getDigitCode(strNum, index, radix) {
+    const delta = 'a'.charCodeAt(0) - 10;
+    const symbol = strNum.charAt(index).toLowerCase();
+    const code = symbol >= '0' && symbol <= '9' ? +symbol : symbol.charCodeAt(0) - delta;
+    return code >= 0 && code < radix ? code : NaN;
+
+}
+//console.log(`string with number ${strNum} with radix ${radix} is ${myParseInt(strNum)}`);
+
+// console.log(eval("let d = function() {return funcion () {return 10}}; Math.sqrt(4) * (100 - d()())"));
+// console.log(` 3 == '3' is ${3 == "3"}`);
+// console.log(` 3 == '3' is ${3 === "3"}`);
+// let n = undefined;
+// console.log(`${(null < '1')} has type ${typeof (n + 2)}`);
+
+function sum (op1 = 10, op2 =20) {
+    return op1 + op2;
+}
+let a1;
+let a2 = 100;
+console.log(`a1 = ${a1}, a2 = ${a2} sum(a1, a2) is ${sum(a1,a2)} `);
