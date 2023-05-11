52c958c4cb90c4d12f430dc831535f825e1e1f91
stanislav-revva <nivadrive1@gmail.com>
2023-05-11 13:05:52 +0300

    coomit 11-05-2023

--- src/app.js
+++ src/app.js
@@ -1,155 +1,54 @@
-// console.log("Helllo World!");
-// //
-// //let a = 5;
-// // a = "hello";
-// // a = false;
-// let a = 5 + "5";
-// console.log(a, typeof a);
-// a = a - 5;
-// console.log(a, typeof a);
-// a = "abc";
-// a /= 2;
-// console.log(a, typeof a);
-// a = "123";
-// // a -= 0;
-// a = +a;
-// console.log(a, typeof a);
-// if (a = 1) {
-//     console.log("a is true");
-// }
-// console.log(a);
-// let a = 1;
-// let b = 3;
-// let c = "2";
-// if (a < b < c) {
-//     console.log("a < b < c is true");
-// }
-// if (2 < "abc") {
-//     console.log('"2" < "abc" is true');
-// }
-// if (2 > "abc") {
-//     console.log('2 > "abc" is true');
-// }
-// if (2 == "abc") {
-//     console.log('2 == "abc" is true');
-// }
-// let a = "abc";
-// a = !!a;
-// console.log(a, typeof a);
-//console.log(2 + 10 * true / false);
-// console.log(3 - (4 + "1"));
-//let a = new Number(3);
-//console.log(a = a + 5, typeof a);00
-// let a = 0.3456789;
-// console.log(a, typeof a);
-// a = a.toFixed(2);
-// console.log(a + 5, typeof a);
-// a = +a;
-// console.log(a + 5, typeof a);
-// a = !!new Number(0);
-// console.log(a, typeof a);
-// a = !!0;
-// console.log(a, typeof a);
-//Math.trunc, Math.round, Math.ceil
-//let a = "256 * 320";
-// console.log(+a);
-// console.log(parseInt(a));
-//a = "257.4a";
-//console.log(+a, parseInt(a), parseFloat(a));
-//console.log(String.fromCharCode(65, 66, 67), String.fromCharCode(65, 66, 67).length);
-// console.log("abc".charCodeAt(0));
-// console.log(String.fromCodePoint(65,66,6));
-//console.log((255).toString(16));
-//console.log(("A" + (+"A") + "A" + "S").toLowerCase());
-
-// function myToStringInteger(number, radix) {
-//     //TODO: number - any number
-//     //radix - computation base, if radix < 2 || radix > 36 then radix = 10
-//     //removes fraction part 5/4, ex - 34.25 = 34; 34.75 = 35;
-//     //toString method is diallowed
-//     //returns string as a view presentation of the integer part of a given number in accordance
-//     const sign = number < 0 ? '-' : '';
-//     number = Math.abs(number);
-//     number = Math.round(number);
-//     if (radix < 2 || radix > 36 ) {
-//         radix = 10;
-//     }
-//     let res = '';
-//     do {
-//         res = getSymbol(number, radix) + res;
-//         number = Math.trunc(number / radix);
-//     } while (number != 0);
-//     return sign + res;
-// }
-// function getSymbol(number, radix) {
-//     const aCode = 'a'.charCodeAt(0);
-//     const delta = aCode - 10;
-//     const remainder = number % radix;
-//     return remainder < 10 ? remainder + '' : String.fromCharCode(remainder + delta);
-// }
-// console.log((123456789).toString(36));
-// console.log(myToStringInteger(123456789, 36));
-// console.log(myToStringInteger(-123456789.1234, 36));
-// console.log(myToStringInteger(0, 36));
-// "string" or 'string' with no string interpolation 'a' - string
-// `...${<expression>}...`
-const strNum = '-0xff';
-let radix;
-//console.log(`string with number ${strNum} with radix ${radix} is ${parseInt(strNum, radix)}`);
-function myParseInt(strNum, radix) {
-    // the same behavior as standard parseInt;
-    strNum = strNum.trim();
-    let index = strNum.charAt(0) == '-' || strNum.charAt(0) == '+' ? 1 : 0;
-    if ((!radix || radix == 16) & getHexdecimalIndex(strNum.substring(index)) > 0) {
-        index += 2;
-        radix = 16;
-    }
-    if (!radix) {
-        radix = 10;
-    }
-    // if (typeof(radix) === 'undefined') {
-    //     radix = 10;
-    //     if (strNum.charAt(index) == '0' && strNum.charAt(index + 1) == 'x') {
-    //         index += 2;
-    //         radix = 16;
-    //     }
-    // }
-    radix = typeof(radix) === 'undefined'? 10 : radix;
-    let res = radix > 1 && radix < 37 ? getDigitCode(strNum, index, radix) : NaN;
-    if(!isNaN(res)) {
-        let digit;
-        index++;
-        while (index < strNum.length && !isNaN(digit = getDigitCode(strNum, index, radix))){
-            res = res * radix + digit;
-            index++;
-        }
-        if(strNum[0] == '-') {
-            res = -res;
-        }
-    }
-    return res;
+// const ar = [];
+// ar[10] = 100;
+// ar[0] = 'hello';
+// ar[3] = [];
+// ar.length = 0;
+// ar[0] = 1;
+// const ar2 = [[1,6], [2,0,0], [3, 1]];
+//add at aray end
+// ar[ar.length] = 10;
+// let s = ar.push(...ar2);
+// ar[10];
+//method "map"
+//console.log([1, 2, 3].map(n=> n ** 2));
+function getRandomIntNumber(min, max, minclusive = true,  maxexclusive = false) {
+    min = minclusive == false ? (min + 1) : min;
+    max = maxexclusive == true ? (max + 1) : max;
+    return min < max ? res = Math.floor(min + Math.random() * (max - min)) : undefined;
 }
-function getHexdecimalIndex(str) {    
-    return str.toLowerCase().startsWith('0x') ? 2 : 0;
+function getArrayRandomIntNumbers(nNumbers, min, max, minclusive = true,
+     maxexclusive = false) {
+    let res = [];
+    res.length = nNumbers > 0 ? nNumbers : 0;
+    return [...res].map(n => getRandomIntNumber(min, max, minclusive = true,
+        maxexclusive = false));
 }
-function getDigitCode(strNum, index, radix) {
-    const delta = 'a'.charCodeAt(0) - 10;
-    const symbol = strNum.charAt(index).toLowerCase();
-    const code = symbol >= '0' && symbol <= '9' ? +symbol : symbol.charCodeAt(0) - delta;
-    return code >= 0 && code < radix ? code : NaN;
-
+//console.log(getRandomIntNumber(1, 10, true, true));
+//console.log(getArrayRandomIntNumbers(5,1,10));
+function getOrderedList(array) {
+    // returns html string of element <ol> items
+    // from a given array elements
+    //example: input - [1, 2, 3]
+    //output "<ol><li>1</li><li>2</li><li>3</li></ol>"
+    return `<ol style="align-items:center; list-style:none">${getListItems(array)}</ol>`
 }
-//console.log(`string with number ${strNum} with radix ${radix} is ${myParseInt(strNum)}`);
-
-// console.log(eval("let d = function() {return funcion () {return 10}}; Math.sqrt(4) * (100 - d()())"));
-// console.log(` 3 == '3' is ${3 == "3"}`);
-// console.log(` 3 == '3' is ${3 === "3"}`);
-// let n = undefined;
-// console.log(`${(null < '1')} has type ${typeof (n + 2)}`);
-
-function sum (op1 = 10, op2 =20) {
-    return op1 + op2;
+function getListItems(array) {
+    return array.map(v => `<li style="width:30px; height:30px; border: solid 1px gray;
+    background-color : ${v? 'black':'white'}  "></li>`).join('');
 }
-let a1;
-let a2 = 100;
-console.log(`a1 = ${a1}, a2 = ${a2} sum(a1, a2) is ${sum(a1,a2)} `);
+function getSquares(array) {
+   
+}
+nNumbers = 10;
+min = 0;
+max = 2;
+array = getArrayRandomIntNumbers(nNumbers,min,max);
+getOrderedList(array);
+//bodyId.innerHTML = getOrderedList(array);
+function getMatrixRandomIntNumbers(rows, columns, min, max){
+    let res = [];
+    res.length = rows;
+    res = [...res];
+    return res.map(n => getArrayRandomIntNumbers(columns, min, max));
+}
+console.log(getMatrixRandomIntNumbers(3,3,1,10));
