5cb2e84b30b732c4fea7dc84afe6dfce83e4de9d
stanislav-revva <nivadrive1@gmail.com>
2023-09-03 11:19:33 +0300

    + limit for jpql queries

--- src/jpql-console.js
+++ src/jpql-console.js
@@ -2,18 +2,21 @@ const givenWords = ['APPLE', 'BRICK' ,'CHOCO'];
 let givenWord;
 const nMoves = 6;
 //elements
-const inputElement = document.getElementById('input-id');
+const inputElementQueryStr = document.getElementById('input-id');
+const inputElementLimit = document.getElementById('input-limit-id');
 const goButtonElement = document.getElementById("go-id");
 const responseField = document.getElementById("response-field");
 const url = 'http://localhost:8080/college';
 //functions
 async function sendJpqlQuery() {
-    const jpqlStr = inputElement.value;
-    if (jpqlStr.length <= 5) {
+    const jpqlStr = inputElementQueryStr.value;
+    const limit = inputElementLimit.value;
+    if (jpqlStr.length <= 5 || parseInt(limit) <= 0) {
         alert('incorrect query');
     } else {
         const jpqlDto = {
             query: jpqlStr,
+            limit: limit
             }
 const response = await fetch(url, {
     method: 'POST',
@@ -22,9 +25,12 @@ const response = await fetch(url, {
     },
     body: JSON.stringify(jpqlDto),
   })
-    const res = await response.json()
+    let res = "incorrect request";
+    if(response.ok){
+        res = await response.json();
+    }
     await printResonse(res);
-    inputElement.value = '';
+    inputElementQueryStr.value = '';
     }
 }
 async function printResonse (response) {
